e3fceef21823701da84725ab741d95157db6e44f
Grayson Myers <graymyers10@gmail.com>
2021-04-12 14:39:50 -0600

    Add migrations and models, according to schema
    Co-authored-by: Aiden Mendez <41342981+aidenmendez@users.noreply.github.com>

---
+++ db/migrate/20210412203003_create_claimants.rb
@@ -0,0 +1,9 @@
+class CreateClaimants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :claimants do |t|
+      t.references :post, foreign_key: true
+      t.references :claimant_id, foreign_key: {to_table: :users}
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,50 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_12_203003) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "claimants", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "claimant_id_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["claimant_id_id"], name: "index_claimants_on_claimant_id_id"
+    t.index ["post_id"], name: "index_claimants_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "tags"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "city"
+    t.string "zip_code"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "claimants", "posts"
+  add_foreign_key "claimants", "users", column: "claimant_id_id"
+  add_foreign_key "posts", "users"
+end
