9bcdcf55be1ea5a9b3c11d2e1f8ef2fb3686d56d
Aiden Mendez <aidenmendez@gmail.com>
2021-04-19 09:48:08 -0600

    Render json response in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
-    if params[:avatar] && user.avatar.attached?
+    if params[:avatar]
       user.avatar.attach(user_params[:avatar])
       user.save!
-    elsif params[:avatar]
-      user.avatar.attach(user_params[:avatar])
-      user.save!
+
+      render json: {"success" => {"message" => "Avatar updated"}}, status:200
+    else
+      render json: {"error" => {"message" => "Image not updated"}}, status:400
     end
   end
 
