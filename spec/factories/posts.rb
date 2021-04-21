2c5745f3f5b537f269d506b25e5f43e821dc8979
Aiden Mendez <aidenmendez@gmail.com>
2021-04-20 18:18:29 -0600

    Update formatting for factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,6 @@ tag_list = ["organic", "homegrown", "pesticide free", "non gmo", "ripe"]
 
 FactoryBot.define do
   factory :post do
-
     user_id { User.pluck(:id).sample }
     title { Faker::Lorem.words(number: 4) }
     description { Faker::Lorem.sentence(word_count: 22) }
